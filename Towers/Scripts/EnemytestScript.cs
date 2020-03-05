4199c595ba35bbaca35bda311c2b12bb42c55f66
GameOfWorldREAL <60042441+GameOfWorldREAL@users.noreply.github.com>
2020-03-05 16:59:56 +0100

    Add files via upload

---
+++ Towers/Scripts/EnemytestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemytestScript : MonoBehaviour
+{
+
+    public float lifeTime;
+    public Rigidbody rb;
+    public float thrust;
+
+    private bool alive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * thrust * 50f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        lifeTime -= .01f;
+    }
+}
---
+++ Towers/Scripts/Slingshot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : Tower
+{
+
+    GameObject slingshotProjectile;
+
+    override protected void Outside_Update()
+    {
+
+    }
+
+    override protected void initiateProjectile()
+    {
+        Vector3 a = new Vector3(0f, 1.35f, 0);
+        slingshotProjectile = Instantiate(projectile, a + transform.position, Quaternion.Euler(0, transform.localEulerAngles.y, 0));
+        slingshotProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * projectileVelocity * Time.deltaTime * 60f * 50f);
+    }
+}
