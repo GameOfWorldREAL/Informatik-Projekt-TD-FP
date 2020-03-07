03309a9df7841b63cc6b7963817ab9b7797e8e18
MinaABP <61802738+MinaABP@users.noreply.github.com>
2020-03-07 12:07:26 +0100

    Add files via upload

--- Anleitung.cs
+++ Anleitung.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Anleitung : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Anleitung : MonoBehaviour { 
 
-    // Update is called once per frame
-    void Update()
+    public void SceneLoader(int scenel)
     {
-        
-    }
+        SceneManager.LoadScene(scenel);
+        }
+
+
 }
