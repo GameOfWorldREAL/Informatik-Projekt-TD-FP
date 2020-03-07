0d87e2be83c85097e39cd14f018074ecfc579feb
Caro915 <60235733+Caro915@users.noreply.github.com>
2020-03-07 09:23:02 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public Vector3 nextPoint;
+    public float speed;
+    public int lifeTime;
+    public float radius = 2;
+    public LayerMask lm;
+    public string tagName = "Temple";
+
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision wurde erkannt");
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, lm, QueryTriggerInteraction.UseGlobal);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Debug.Log("for");
+            if (hitColliders[i].gameObject.GetComponent<Temple>().tag == tagName)
+            {
+                Debug.Log("Es ist ein Tempel");
+                hitColliders[i].gameObject.GetComponent<Temple>().SubHealth(damage);
+            }
+        }
+    }
+    public void SubEnemyHealth()                //für Projektile, die den Enemy treffen; zieht Lebenspunkte ab
+    {
+            health -= health;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("ja");
+        if (health==0)                          //Absicherung, dass Enemy zerstört wird
+        {
+            Destroy(gameObject);
+            Debug.Log("Enemy hat kein Leben mehr");
+        }
+        Destroy(gameObject, lifeTime);          //das GameObjekt wird nach einer gewissen Zeit zerstört (falls es in der Map festhängt)
+        this.transform.position += nextPoint * speed * Time.deltaTime; //Bewegung des Enemys
+    }
+}
---
+++ Temple.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple : MonoBehaviour
+{
+    //public GameObject temple;
+    public int health = 200; //Gesundheit 
+    //private int wellenanzahl;
+    //private int gegneranzahl;
+    
+    //public void SubGegneranzahl(int anzahl)
+    //{
+    //    gegneranzahl -= anzahl;
+    //    if (gegneranzahl==0)
+    //    {
+            //Starte nächste Welle
+    //    }
+    //}
+    //public void StartNextWave()
+    //{
+    //    if (wellenanzahl > 0)
+    //    {
+            //SendMessage an Spawnppunkte neue Welle zu starten
+    //        wellenanzahl--;
+    //    }
+    //    else 
+    //    {
+    //        EndGame();
+    //    }
+    //}
+    public void EndGame() 
+    {
+        //Szenenwechsel zu Startbildschirm
+    }
+    public void SubHealth(int damage)
+    {
+        health -= damage;
+        if (health <= 0)  //Wenn der Tempel zerstört ist
+        {
+            EndGame();
+        }
+    }
+    void Start()
+    {
+        
+        //Lasse Gegneranzahl von Welle übergeben
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        //Bezahlsystem
+    }
+}
